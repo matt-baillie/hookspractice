c5ce090e27f92b5757a0a54752c7183ee5c3c93c
Matthew Baillie <mbaillie93@gmail.com>
2022-10-30 17:23:53 -0700

    useContext part 1

--- src/components/UseContext.js
+++ src/components/UseContext.js
@@ -1,5 +1,21 @@
-import React from "react";
+import React, { useState } from "react";
+import { FunctionContextComponent } from "./contextComponents/FunctionContextComponent";
+import ClassContextComponent from "./contextComponents/ClassContextComponent";
+
+export const ThemeContext = React.createContext();
 
 export const UseContext = () => {
-  return <div>UseContext</div>;
+  const [darkTheme, setDarkTheme] = useState(true);
+  function toggleTheme() {
+    setDarkTheme((prevDarkTheme) => !prevDarkTheme);
+  }
+  return (
+    <>
+      <ThemeContext.Provider value={darkTheme}>
+        <button onClick={toggleTheme}>Toggle Theme</button>
+        <FunctionContextComponent />
+        <ClassContextComponent />
+      </ThemeContext.Provider>
+    </>
+  );
 };
